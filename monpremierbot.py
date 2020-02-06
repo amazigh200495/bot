cf23e255819f936c159b22246ce90fd5d1a563ce
amazigh200495 <amazigh200495@gmail.com>
2020-02-06 17:03:33 +0100

    MyBot 2.0

--- monpremierbot.py
+++ monpremierbot.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 import random
 import re
+import datetime
 
-#salutation = ['salut','Bonjour', 'Bonsoir', 'Azul']
-#question = input("Bonjour je suis votre premier bot,\n ");
+category = [ "pc","ordinateur" , "telephone" , "voiture" ] 
+exclure = ["je","vous","la","le","les","une","un","des"]
+requete =input ("Bonjour, je suis votre assisstant MyBot, Que desirez-vous acheter ?\n")
+decoupe=(re.findall(r"[a-zA-Z]+", requete))
+#print(decoupe)
+final = list(set(decoupe) - set(exclure))
+finale = [mot.lower() for mot in final if len(mot) > 2 ]
+#print (finale)
+if ("voiture" in finale):
+   print ("voici le catalogue des voitures")
+elif ("telephone" in finale):
+   print ("voici le catelogue des telephones")
+elif ("ordinateur" or "pc" in finale):
+   print ("voici le catalogue des ordinateurs")
+else:
+   print("Vu le {0}".format(datetime.datetime.now()))
 
-#proposition = ["salut","bonjour","bonsoir","azul"]
-
-#if (question.lower() in proposition):
-#   print (random.choice(salutation) + " mon ami")
-#else:
-#   print("je ne comprend pas ")
-
-re.findall(r"[a-zA-Z]+", question)
