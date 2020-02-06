49dcb0df5ed8d7e3a2f6555b063927aee57d1262
amazigh200495 <amazigh200495@gmail.com>
2020-02-06 16:22:32 +0100

    Mon premier bot

---
+++ monpremierbot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+import re
+
+#salutation = ['salut','Bonjour', 'Bonsoir', 'Azul']
+#question = input("Bonjour je suis votre premier bot,\n ");
+
+#proposition = ["salut","bonjour","bonsoir","azul"]
+
+#if (question.lower() in proposition):
+#   print (random.choice(salutation) + " mon ami")
+#else:
+#   print("je ne comprend pas ")
+
+re.findall(r"[a-zA-Z]+", question)
